4ce842b664b5e9f77540323fd8f2ab9b0738d463
ctejadah97 <ctejadah97@gmail.com>
2021-08-31 20:54:55 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -7,14 +7,3 @@ import { IUser } from './core/interfaces/user.interface';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-const client: IUser = {
-  names: 'Cristian',
-  lastnames: 'Tejada Hernandez',
-  email: 'ctejada@cuc.edu.co',
-  phones: [
-    { type: 'mobile', number: '3652254415' },
-    { type: 'home', number: '3651765' }
-  ]
-};
-
-console.log(client);
